4b3d48283d6f6da37170778cd4f2702c5dc013c3
Yago Lizarribar <yago.lizarribar@gmail.com>
2026-03-24 19:08:04 +0100

    simplify feature engineering

--- src/mapcharting/model/model.py
+++ src/mapcharting/model/model.py
@@ -22,29 +22,22 @@ class ChannelCharter(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.feature = FeatureEngineeringLayer()
         self.fc1 = nn.Sequential(
-            nn.Linear(26624, 1024), nn.ReLU(), nn.BatchNorm1d(1024)
+            nn.Linear(416, 256), nn.ReLU(), nn.BatchNorm1d(256)
         )
         self.fc2 = nn.Sequential(
-            nn.Linear(1024, 512), nn.ReLU(), nn.BatchNorm1d(512)
-        )
-        self.fc3 = nn.Sequential(
-            nn.Linear(512, 256), nn.ReLU(), nn.BatchNorm1d(256)
-        )
-        # self.fc4 = nn.Sequential(
-        #     nn.Linear(256, 128), nn.ReLU(), nn.BatchNorm1d(128)
-        # )
-        self.fc5 = nn.Sequential(
             nn.Linear(256, 64), nn.ReLU(), nn.BatchNorm1d(64)
         )
-        self.encoder = nn.Sequential(
-            self.fc1, self.fc2, self.fc3, self.fc5,
+        self.fc3 = nn.Sequential(
+            nn.Linear(64, 16), nn.ReLU(), nn.BatchNorm1d(16)
         )
-        self.output_layer = nn.Linear(64, 2)
+        self.encoder = nn.Sequential(
+            self.fc1, self.fc2, self.fc3,
+        )
+        self.output_layer = nn.Linear(16, 2)
 
     def forward(self, X):
-        X = self.feature(X)
+        X = torch.abs(X)
         X = X.flatten(start_dim=1)
         X = self.encoder(X)
         return self.output_layer(X)
